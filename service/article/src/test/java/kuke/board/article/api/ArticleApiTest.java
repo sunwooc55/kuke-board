b1895520e39137d0455154628fb2af427934d314
sunwooc55 <sunwooc55@gmail.com>
2025-12-23 19:56:48 +0900

    2025.12.23 19:56

---
+++ service/article/src/test/java/kuke/board/article/api/ArticleApiTest.java
@@ -0,0 +1,79 @@
+package kuke.board.article.api;
+
+import kuke.board.article.service.request.ArticleCreateRequest;
+import kuke.board.article.service.response.ArticleResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class ArticleApiTest {
+    // http://localhost:9000 주소로 떠 있는 서버에 요청을 보낼 준비를 한다.
+    RestClient restClient = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest(){
+        ArticleResponse response = create(new ArticleCreateRequest(
+                "hi", "my content", 1L, 1L
+        ));
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse create(ArticleCreateRequest request){
+        return restClient.post()
+                .uri("/v1/articles")
+                .body(request)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void readTest(){
+        ArticleResponse response = read(261807201869377536L);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse read(Long articleId){
+        return restClient.get()
+                .uri("/v1/articles/{articleId}", articleId)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void updateTest(){
+        update(261807201869377536L);
+        ArticleResponse response = read(261807201869377536L);
+        System.out.println("response = " + response);
+    }
+
+    void update(Long articleId){
+        restClient.put()
+                .uri("/v1/articles/{articleId}", articleId)
+                .body(new ArticleUpdateRequest("hi 2", "my content 22"))
+                .retrieve();
+    }
+
+    @Test
+    void deleteTest(){
+        restClient.delete()
+                .uri("/v1/articles/{articleId}", 261807201869377536L)
+                .retrieve();
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleCreateRequest{
+        private String title;
+        private String content;
+        private Long writerId;
+        private Long boardId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleUpdateRequest{
+        private String title;
+        private String content;
+    }
+}
